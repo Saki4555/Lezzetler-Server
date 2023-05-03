6e9ff3cb6f260608994a53c74df034f743ce1833
saki4555 <saki999v@gmail.com>
2023-05-03 21:11:11 +0600

    cors installed and fixed fetch problem

--- index.js
+++ index.js
@@ -2,8 +2,23 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 
+const cors = require('cors');
+app.use(cors());
+
+const chefs = require('./data/chef.json');
+
 app.get('/', (req, res) =>{
-    res.send('boys');
+    res.send('hello');
+});
+app.get('/chefs', (req, res) =>{
+    res.send(chefs);
+});
+
+app.get('/chefs/:id', (req, res) =>{
+    const id = parseInt(req.params.id);
+    const choosedChef = chefs.find( chef => chef.id === id);
+    console.log(id);
+    res.send(choosedChef);
 });
 
 app.listen(port, () =>{
